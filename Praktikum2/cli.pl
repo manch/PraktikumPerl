ba13e93f9e136a24b25a3a627a48f56ea00f66a2
Christian Manus <c.manus@gmx.de>
2012-04-24 11:01:54 +0200

    Prak2 first fininsh state

--- Praktikum2/cli.pl
+++ Praktikum2/cli.pl
@@ -20,7 +20,6 @@ while(<stdin>){
 	next;
     }
     elsif($input eq "l"){
-
 	list();
 	print "(AdressVerwaltung) ";
 	next;
@@ -32,23 +31,22 @@ while(<stdin>){
     }
     @inputArray = split(" ", $input);
     if ($inputArray[0] eq "a"){
-	print "Das ist append zu nn\n";
+	appendAdress($inputArray[1]);
 	print "(AdressVerwaltung) ";
 	next;
     }
     elsif ($inputArray[0] eq "d"){
-	print "Das löscht nn\n";
+	deleteById($inputArray[1]);
 	print "(AdressVerwaltung) ";
 	next;
     }
-    elsif ($inputArray[0] eq "l"){
-	
+    elsif ($inputArray[0] eq "l"){	
 	listById($inputArray[1]);
 	print "(AdressVerwaltung) ";
 	next;
     }
     elsif ($inputArray[0] eq "s"){
-	print "such test ... in allen\n";
+	searchText($inputArray[1]);
 	print "(AdressVerwaltung) ";
 	next;
     }
@@ -87,7 +85,7 @@ sub list{
 
 sub browseHelp{
     print "e\tAdd a new adress\n\tForm: 'key:value'\n\tLast item form: '.key:value'\n\n";
-    print "a id\tChange or add an adress item: 'key:value'\n\n";
+    print "a id\tChange or add an adress item: 'key:value'\tLast item with: '.key:value'\n\n";
     print "d id\tDelete adress on this id\n\n";
     print "l id\tBrowse adress on the given id formated\n\n";
     print "l\tBrowse all adresses\n\n";
@@ -107,4 +105,84 @@ sub listById{
 	    print "\n";
 	}
     }
+}
+
+sub appendAdress{
+    my ($curId) = @_;
+    print "The adress you want to change:\n\n";
+    listById($curId);
+    print "(appendAdress) ";
+    while (<stdin>){
+	chomp($input = $_);
+	if(substr($input,0,1) eq "."){
+	    my @tmp = split(//,$input);
+	    shift @tmp;
+	    $input = join "",@tmp;
+	    @inputArray = split(":",$input);
+	    foreach my $k1 ( sort keys %adresses ) {
+		foreach my $k2 ( sort keys %{$adresses{$k1}} ) {
+		    if($curId eq $k2){
+			foreach my $k3 ( sort keys %{$adresses{$k1}{$k2}} ) {
+			    if ($k3 eq $inputArray[0]){
+				delete $adresses{$k1}{$k2}{$k3};
+				$adresses{"ID"}{"$curId"}{"$inputArray[0]"}= $inputArray[1];
+			    }
+			    else{
+				$adresses{"ID"}{"$curId"}{"$inputArray[0]"}= $inputArray[1];
+			    }
+			}
+		    }
+		}
+	    }
+	    last;
+	}
+	@inputArray = split(":",$input);
+
+	foreach my $k1 ( sort keys %adresses ) {
+	    foreach my $k2 ( sort keys %{$adresses{$k1}} ) {
+		if($curId eq $k2){
+		    foreach my $k3 ( sort keys %{$adresses{$k1}{$k2}} ) {
+		        if ($k3 eq $inputArray[0]){
+			    delete $adresses{$k1}{$k2}{$k3};
+			    $adresses{"ID"}{"$curId"}{"$inputArray[0]"}= $inputArray[1];
+		        }
+		        else{
+			    $adresses{"ID"}{"$curId"}{"$inputArray[0]"}= $inputArray[1];
+		        }
+		    }
+		}
+	    }
+	}
+	print "(appendAdress) ";
+    }    
+}
+
+sub deleteById{
+    my ($curId) = @_;
+    foreach my $k1 ( sort keys %adresses ) {
+	foreach my $k2 ( sort keys %{$adresses{$k1}} ) {
+	    if ($curId eq $k2){
+		foreach my $k3 ( sort keys %{$adresses{$k1}{$k2}} ) {
+    		    print "$k1\t$k2\t$k3\t$adresses{$k1}{$k2}{$k3}\n";
+		    delete $adresses{$k1}{$k2}{$k3};
+		}
+		print "Deleted\n";
+	    }
+	}
+    }
+}
+
+sub searchText{
+    my ($searchText) = @_;
+    foreach my $k1 ( sort keys %adresses ) {
+	foreach my $k2 ( sort keys %{$adresses{$k1}} ) {
+	    foreach my $k3 ( sort keys %{$adresses{$k1}{$k2}} ) {
+		if($adresses{$k1}{$k2}{$k3} =~ $searchText){
+		    listById($k2);
+		}
+	    }
+	    print "\n";
+	}
+    }
+
 }
